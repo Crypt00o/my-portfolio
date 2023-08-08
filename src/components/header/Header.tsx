3cde63004ee02694b7eff14257fee147ad36cc2f
Crypt00o <0xCrypt00o@protonmail.com>
2023-08-08 22:09:34 +0300

    add IntroText component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import IntroText from "./IntroText";
 import ClickToAction from "./ClickToAction";
 import "./Header.css";
 
 const Header: React.FC = () => (
 	<header>
 		<div className="container header-container">
+			<IntroText />
 			<ClickToAction />
 		</div>
 	</header>
