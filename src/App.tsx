2f06bdf96ab03ef2d12d5c6bdadc566fe3112254
Crypt00o <0xCrypt00o@protonmail.com>
2023-08-08 08:30:09 +0300

    construct App.tsx from components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import './App.css'
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import Portfolio from './components/portfolio/Portfolio';
+import Services from './components/services/Services';
+import Experience from './components/experience/Experience';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+
 
 const App = () => (
 	<>
+		<Header />
+		<Nav />
+		<About />
+		<Experience />
+		<Services />
+		<Portfolio />
+		<Contact />
+		<Footer />
 	</>
 );
 
 
-export default App
+export default App;
