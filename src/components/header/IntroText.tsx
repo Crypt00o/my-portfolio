42f2abe5b44c5fa3f2324a0a401d0f4c98e40266
Crypt00o <0xCrypt00o@protonmail.com>
2023-08-08 22:27:53 +0300

    update IntroText component

--- src/components/header/IntroText.tsx
+++ src/components/header/IntroText.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const IntroText: React.FC = () => (
 	<>
-		<h5>Hey There !</h5>
+		<h3>Hey There !</h3>
 		<h4> I 'm</h4>
 		<h1> Eslam Muhammed </h1>
 		<h2 className="text-light"> Full Stack Developer</h2>
